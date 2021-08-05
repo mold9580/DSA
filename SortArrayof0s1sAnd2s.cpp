0f658029cdb0722ea8a37bbc2e6f93e9d8136ae6
mold9580 <bantysingh9580@gmail.com>
2021-08-05 20:16:14 +0530

    Sorted the array in one pass

--- SortArrayof0s1sAnd2s.cpp
+++ SortArrayof0s1sAnd2s.cpp
@@ -2,6 +2,52 @@
 #include<iostream>
 using namespace std;
 int n;
+void sort012InOnePass(int arr[])
+{
+    int i=0,j=0,k,temp;
+    while(arr[i]==0)
+    {
+        i++;
+    }
+    i--;
+
+
+    j=i+1;
+    while(arr[j]==1)
+    {
+        j++;
+    }
+    j--;
+
+
+    k=n-1;
+    while(arr[k]==2)
+    {
+        k--;
+    }
+    k++;
+
+    temp=j+1;
+    while(temp<k)
+    {
+        if(arr[temp]==1)
+        {
+            j++;
+            temp++;
+        }
+        else if(arr[temp]==0)
+        {
+            swap(arr[i+1],arr[temp]);
+            i++;
+            temp++;
+        }
+        else
+        {
+            swap(arr[k-1],arr[temp]);
+            k--;
+        }
+    }
+}
 void sort012(int *arr)
 {
      int a[3]={0,0,0};
@@ -34,6 +80,7 @@ int main()
 {
     int arr[]={0,1,1,2,2,1,1,0};
     n=sizeof(arr)/sizeof(int);
-    sort012(arr);
+    sort012InOnePass(arr);
+    //sort012(arr);
     cout<<arr;
 }
