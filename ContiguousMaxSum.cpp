303c9948498d62c2b4222f9ef32a4b78cb1d2a4f
mold9580 <bantysingh9580@gmail.com>
2021-08-11 21:10:22 +0530

    Kadane's Algorithm

---
+++ ContiguousMaxSum.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+int MaxSum(vector<int> &v)
+{
+    int maxsum=0;
+    int currsum=0;
+    for(int i=0;i<v.size();i++)
+    {
+        currsum+=v[i];
+        if(currsum<0)
+        {
+            currsum=0;
+        }
+        else
+        {
+            maxsum=max(currsum,maxsum);
+        }
+    }
+    return maxsum;
+}
+signed main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+    {
+        int temp;
+        cin>>temp;
+        v.push_back(temp);
+    }
+    cout<<MaxSum(v);
+    return 0;
+}
