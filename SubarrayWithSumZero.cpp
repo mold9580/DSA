4b590feb6fed394c02a7cbe31c0aa8f368ec5090
mold9580 <bantysingh9580@gmail.com>
2021-08-20 13:55:36 +0530

    Subarray with 0 sum, Time Complexity:O(n) Space Complexity:O(n)

---
+++ SubarrayWithSumZero.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool subArrayExists(int arr[], int n)
+{
+    if(n==1)
+    {
+        if(arr[0]==0) return true;
+        else return false;
+    }
+    unordered_map<int,int>m;
+    m[arr[0]]=0;
+    m[0]=0;
+    int arr1[n];
+    arr1[0]=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        arr1[i]=arr[i]+arr1[i-1];
+        if(m.find(arr1[i])!=m.end()) return true;
+        else m[arr1[i]]=0;
+    }
+    return false;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    if(subArrayExists(arr,n)) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+}
