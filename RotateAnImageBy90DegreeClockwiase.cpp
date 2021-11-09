069f2d55b616807d776c3f3ac53ddf2cf044525c
mold9580 <bantysingh9580@gmail.com>
2021-11-09 13:13:55 +0530

    Rotate an image by 90 Degree clock-wise. Space Complexity:O(1), Time ComplexityO(n^2)

---
+++ RotateAnImageBy90DegreeClockwiase.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+class Solution
+{   
+    public:
+    void rotateby90(vector<vector<int> >& matrix, int n) 
+    { 
+        for(int j=0;j<n;j++)
+        {
+            //reverse all the colums
+            for(int i=0;i<n/2;i++) swap(matrix[i][j],matrix[n-1-i][j]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            //take the transpose of reverse matrix
+            for(int j=0;j<i;j++) swap(matrix[i][j],matrix[j][i]);
+        }
+        //An alternative way is to rotate n/2 squares 90 Degree clockwise
+        //an square in matrix can be considered as square formed by rows and columns of matrix
+    } 
+};
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.rotateby90(matrix, n);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                cout<<matrix[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}  
