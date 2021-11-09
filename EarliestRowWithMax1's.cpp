d02c02fcc3289e7d2c5737c47419b01b6c7d0ca3
mold9580 <bantysingh9580@gmail.com>
2021-11-09 13:24:36 +0530

    Firstest row with maximum number of 1's. Space Complexity:O(1), Time Complexity:O(n+m).

---
+++ EarliestRowWithMax1's.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) 
+	{
+	    int i=0,j=m-1,ans=-1;
+	    while(i<n and j>=0)
+	    {
+	        if(arr[i][j]==1) j--,ans=i;
+	        else i++;
+	        
+	        if(j==-1) break;
+	    }
+	 return ans;   
+	}
+
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector< vector<int> > arr(n,vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin>>arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
